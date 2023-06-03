a8134dc620030620e2903636e2761048c55973c1
Alan <74235604+Elsefai@users.noreply.github.com>
2023-06-03 14:58:07 +0600

    Initial Commit

---
+++ Converter Currency/Converter_CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Converter_CurrencyApp.swift
+//  Converter Currency
+//
+//  Created by Alan Zhussupov on 03.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Converter_CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
